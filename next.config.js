3d3cbeccfe1ac977c49f7abff74180abb7c9ad0c
Chema Balsas <jose.balsas@liferay.com>
2021-08-03 11:52:20 +0100

    chore: migrate to ts and add eslint

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	eslint: {
+		dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
+	future: {
+		webpack5: true
+	}
+}
